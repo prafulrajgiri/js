0299708a69ac06a0e7e987a1bcf787887238f797
Praful Raj Giri <124333068+prafulrajgiri@users.noreply.github.com>
2024-10-15 14:20:38 +0000

    Variable

---
+++ Basics/01_Variables.js
@@ -0,0 +1,17 @@
+const accountId = 28
+let accountEmail = "Praful@gmail.com"
+var accountPass = "12322"
+accountCountry = "Nepal"
+/* 
+Prefer not using var in your program 
+*/
+let accountNum
+
+// accountId = 2
+accountEmail = "prg@prg.com"
+accountPass = "123"
+accountCountry = "Bhutan"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountCountry, accountPass, accountNum])
