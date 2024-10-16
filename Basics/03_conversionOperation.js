dcfa6e68e30018d039f56a21b3bbb37cee945bdf
Praful Raj Giri <124333068+prafulrajgiri@users.noreply.github.com>
2024-10-16 13:17:36 +0000

    Conversion of Datatypes

---
+++ Basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+//  let score = "50"
+//  let score = "50abc"
+// let score = null
+// let score = undefined
+// let score = true
+
+//  console.log(typeof score);
+ 
+//  let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+//  console.log( valueInNumber);
+
+//  // "50" => 50
+ // "50abc" => Nan
+ // true => 1; false =>0
+
+//  let isLoggedIn = "Praful"
+
+//  let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(typeof booleanIsLoggedIn)
+//  console.log(booleanIsLoggedIn)
+ let someNumber = 333
+
+ let stringValue = String(someNumber)
+ console.log(stringValue)
+ console.log(typeof stringValue)
+ 
