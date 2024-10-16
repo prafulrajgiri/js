bb581479473d9c6d38ea47d0f3bb8bb592f8cc81
Praful Raj Giri <124333068+prafulrajgiri@users.noreply.github.com>
2024-10-16 14:41:24 +0000

    Conversion of Datatypes

--- Basics/03_conversionOperation.js
+++ Basics/03_conversionOperation.js
@@ -19,9 +19,43 @@
 //  let booleanIsLoggedIn = Boolean(isLoggedIn)
 //  console.log(typeof booleanIsLoggedIn)
 //  console.log(booleanIsLoggedIn)
- let someNumber = 333
+//  let someNumber = 333
 
- let stringValue = String(someNumber)
- console.log(stringValue)
- console.log(typeof stringValue)
- 
+//  let stringValue = String(someNumber)
+//  console.log(stringValue)
+//  console.log(typeof stringValue)
+
+ // ****** Operations ****** 
+
+ let value = 2
+ let negvalue = -value
+//  console.log(negvalue)
+
+//  console.log(value+value)
+//  console.log(value/value)
+//  console.log(value-value)
+//  console.log(value*value)
+//  console.log(value**3)
+//  console.log(value%value)
+let str1 = "Hello "
+let str2 = "Praful"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+//  console.log(true)
+//  console.log(+true)
+//  console.log(+"")
+//Pre-increment operator
+// let x = 1
+// let y = ++x
+// console.log('x: ' + x + ' y: ' + y)
+
+//post-increment operator
+// let x = 1
+// let y = x++
+// console.log(`X: ${x} Y: ${y}`)
