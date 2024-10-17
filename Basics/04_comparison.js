51fbc2be013c84daf1e3289bd35ea3c567cddde3
Praful Raj Giri <124333068+prafulrajgiri@users.noreply.github.com>
2024-10-17 12:25:31 +0000

    Comparison Operator

---
+++ Basics/04_comparison.js
@@ -0,0 +1,14 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2">1)
+// console.log("02">1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+// The reason that an equality check == and comparisons < > <= >= work differently. Comparisons convert null to a number, treating it as 0.
+console.log("2" === 2)
