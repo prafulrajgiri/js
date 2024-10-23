0b4daa9246d6e2a27f5f06d32686adc9966fb36a
Praful Raj Giri <124333068+prafulrajgiri@users.noreply.github.com>
2024-10-23 15:55:16 +0000

    Numbers and Maths

---
+++ Basics/07_Numbers_and_Maths.js
@@ -0,0 +1,27 @@
+// const score = 200
+// console.log(score)
+
+// const scoreTwo = new Number(150)
+// console.log(scoreTwo)
+
+// console.log(scoreTwo.toString().length)
+// console.log(scoreTwo.toFixed(2))
+
+// const balance = 1345.54
+// console.log(balance.toPrecision(4))
+
+// const balance2= 2000000
+// console.log(balance2.toLocaleString('en-IN'))
+
+
+//********* Maths ***********
+
+// console.log(Math)
+// console.log(Math.abs(-12))
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(5.2));
+// console.log(Math.max(2,4,5,6,7))
+// console.log(Math.min(2,4,5,6,7))
+
+console.log(Math.random()*10)
