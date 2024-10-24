a2265a0ff2a83ac944792740647b04ee7a0b989b
Praful Raj Giri <124333068+prafulrajgiri@users.noreply.github.com>
2024-10-24 14:00:03 +0000

    Date and TIme

---
+++ Basics/08_Date_and_Time.js
@@ -0,0 +1,30 @@
+ //Dates
+
+ let currentDate = new Date()
+//  console.log(currentDate)
+// console.log(currentDate.toString())
+// console.log(currentDate.toDateString())
+// console.log(currentDate.toLocaleString())
+// console.log(typeof currentDate)
+
+
+// let createdDate = new Date(2022,5,22)
+// console.log(createdDate.toDateString())
+
+// let createdDate = new Date(2022,5,22,10,20)
+// console.log(createdDate.toLocaleString())
+
+// let createdDate = new Date("2023-01-14")
+// console.log(createdDate.toLocaleString())
+
+let createdDate = new Date("01-14-2023")
+// console.log(createdDate.toLocaleString())
+
+let TimeStamp = Date.now()
+
+// console.log(TimeStamp)
+// console.log(createdDate.getTime())
+// console.log(Math.floor(Date.now()/1000)) 
+
+
+
