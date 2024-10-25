02f973389826105e379fe30e115a40ec841b3828
Praful Raj Giri <124333068+prafulrajgiri@users.noreply.github.com>
2024-10-25 13:37:21 +0000

    Array

---
+++ 01_array.js
@@ -0,0 +1,32 @@
+// array
+
+// const myArr = [0,1,2,3,4,5]
+// const superHeroes = ["IronMan","Hulk","Thor"]
+
+// const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[1])
+
+// Array Methods
+// myArr.push(6)
+// console.log(myArr)
+// myArr.pop()
+// console.log(myArr)
+// myArr.unshift(1)
+// console.log(myArr)
+// myArr.shift()
+// console.log(myArr)
+// console.log(myArr.includes(9))
+// const newArr = myArr.join()
+// console.log(newArr)
+// console.log(typeof newArr)
+
+// slice, splice
+
+const arr = [0,1,2,3,4,5,]
+console.log("Original Array:",arr)
+
+console.log(arr.slice(1,3))
+console.log("Slice:",arr)
+
+console.log(arr.splice(1,3))
+console.log("Splice",arr)
