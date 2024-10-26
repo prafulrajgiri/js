313ee21754624ddefee8719504798a8292d4ed8d
Praful Raj Giri <124333068+prafulrajgiri@users.noreply.github.com>
2024-10-26 13:06:00 +0000

    Arrays

---
+++ 02_array.js
@@ -0,0 +1,29 @@
+const heroNames = ["IronMan","Thor","SpiderMan"]
+const updateName = ["Hulk","Captain America", "DrStrange"]
+
+// heroNames.push(updateName)
+
+// console.log(heroNames)
+// console.log(heroNames[3][2])
+
+//Concat: Returns New Array
+// const updatedHeroes = heroNames.concat(updateName)
+// console.log(updatedHeroes)
+
+//Spread
+// const allHeroes = [...heroNames,...updateName]
+// console.log(allHeroes);
+
+// const array = [1,2,3,[4,5,6],6,[7,8,[9,10],11],12]
+// const arrayFlat = array.flat(2)
+// console.log(arrayFlat)
+
+console.log(Array.isArray("Praful"))
+console.log(Array.from("Praful"))
+
+const list1 = 123
+const list2 = 456
+const list3 = 789
+console.log(Array.of(list1,list2,list3))
+
+
