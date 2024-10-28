1b73c0b9e0f6dfdac12c60308f79f293225e854f
Praful Raj Giri <124333068+prafulrajgiri@users.noreply.github.com>
2024-10-28 16:20:22 +0000

    Function

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -21,4 +21,41 @@ function addNumber(num1,num2){
 }
 
 const result = addNumber(432,21)
-console.log(result)
+// console.log(result)
+
+function addCartSum(...value){
+    let sum = null
+    for (let i = 0; i < value.length; i++) {
+         sum = value[i] + sum
+        }
+    console.log("Total:",sum)
+}
+
+// addCartSum(123,400,980,765,433,234,)
+
+// function spreadTest(num1,num2,...value){
+//     console.log(num1,num2,value)
+// }
+// spreadTest(2,3,4,5,6)
+
+const userData = {
+    name: "Praful",
+    subscription: "Premium",
+    duration: 45
+}
+
+function userInfo(objectPass){
+    console.log(`${objectPass.name}, You are using ${objectPass.subscription} version and the duration left is ${objectPass.duration} days`)
+}
+// userInfo(userData)
+// userInfo({name:"ram",subscription:"Base",duration: 0})
+
+
+const arr = [1,3,5,6,7]
+
+function secondValue(arrayPass){
+    return arrayPass[1]
+}
+
+// console.log(secondValue(arr))
+console.log(secondValue([2,1,4,5,6]))
