fda3339f31ffc95378efbdcf81170fd95b305e47
Praful Raj Giri <124333068+prafulrajgiri@users.noreply.github.com>
2024-10-28 12:15:00 +0000

    Function

---
+++ 03_Basics/01_Functions.js
@@ -0,0 +1,24 @@
+//  function greetUser(){
+//     console.log("Hello")
+//  }
+
+//  greetUser()
+
+// function greetUser(username){
+//     return `Hello ${username} `
+// }
+
+// console.log(greetUser("Praful"))
+
+// function greetUser(username = "User"){
+//     return `Hello ${username} `
+// }
+
+// console.log(greetUser("Praful"))
+
+function addNumber(num1,num2){
+    return num1 + num2
+}
+
+const result = addNumber(432,21)
+console.log(result)
