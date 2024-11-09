aa0da2faaf43453528dd85a5555f4c482c0d55be
Praful Raj Giri <124333068+prafulrajgiri@users.noreply.github.com>
2024-11-09 14:44:03 +0000

    Iteration in JS

---
+++ 04_Control_Flow/iteration.js
@@ -0,0 +1,26 @@
+// for (let i = 0; i < 10; i++) {
+//     console.log(`${i} square  : ${i*i}`);
+    
+// // }
+
+// const array = [2,3,4,5,6,3,4,5]
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i])  
+// }
+
+// for (let i = 0; i < 5; i++) {
+//     if (i==3) {
+//         console.log("number 3 detected");
+//         continue    
+//     }
+//     console.log(i);
+    
+// }
+// for (let i = 0; i < 5; i++) {
+//     if (i==3) {
+//         console.log("number 3 detected");
+//         break   
+//     }
+//     console.log(i);
+    
+// }
